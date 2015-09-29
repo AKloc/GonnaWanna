d7c6e8537eb376efeb7a4c8505afa07e0868abaf
Andy Kloc <andy.kloc@gmail.com>
2015-09-29 15:38:45 -0400

    Login token is coming back, need to fix Callback error from it, though.

--- GonnaWanna/Scripts/Site/factories.js
+++ GonnaWanna/Scripts/Site/factories.js
@@ -1,11 +1,12 @@
 ﻿var gonnaWannaApp = angular.module('gonnaWannaApp');
 
-gonnaWannaApp.service("UserAccountService", function ($http) {
+gonnaWannaApp.factory("UserAccountFactory", function ($http) {
     
     var viewModel = this;
     //$scope.viewModel = this;
     viewModel.isLoggedIn = false;
     viewModel.isRegistered = false;
+    viewModel.token = "";
     viewModel.UserData =
         {
             FirstName: "testFirstName",
@@ -16,91 +17,125 @@ gonnaWannaApp.service("UserAccountService", function ($http) {
             ConfirmPassword: "Pass_1",
         };
 
-    viewModel.RegisterUser = function () {
+    viewModel.RegisterUser = function (callBack) {
         $http.post("/api/account/register", viewModel.UserData).success(function (data) {
-            return data;
-            //$scope.returnData = data;
+            callBack(data);
         });
     };
 
-    viewModel.LoginUser = function () {
+    viewModel.LoginUser = function (callBack) {
         // Format the URL string.
-        urlParams = "grant_type=password&username=" + UserName + "&password=" + Password;
+        urlParams = "grant_type=password&username=" + viewModel.UserData.UserName + "&password=" + viewModel.UserData.Password;
 
-        $http.post("/api/token", urlParams, {
+        $http.post("/token", urlParams, {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
         }).success(function (data) {
-            //$scope.returnData = data;
+            viewModel.token = data.access_token;
+            viewModel.isLoggedIn = true;
+            callBack(data);
+        }).error(function (data) {
+            var dadsf = 3;
         });
     };
+
+    
+
+    /*
+        var _login = function (loginData) {
+
+        var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+
+        var deferred = $q.defer();
+
+        $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+
+            localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName });
+
+            _authentication.isAuth = true;
+            _authentication.userName = loginData.userName;
+
+            deferred.resolve(response);
+
+        }).error(function (err, status) {
+            _logOut();
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+
+    };
+    */
+    
 
 
     return viewModel;
-
     //        $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
-
 });
 
-gonnaWannaApp.service("EventService", function ($http) {
-    var eventService = this;
 
-    eventService.GetAllEvents = function (callBack) {
+
+
+
+gonnaWannaApp.factory("EventFactory", function ($http) {
+    var eventFactory = this;
+
+    eventFactory.GetAllEvents = function (callBack) {
         $http.get("/api/events").success(function (data) {
             callBack(data);
         });
     };
 
-    eventService.GetEvent = function (eventID, callBack) {
+    eventFactory.GetEvent = function (eventID, callBack) {
         $http.get("/api/events/" + eventID).success(function (data) {
             callBack(data);
         });
     };
 
-    return eventService;
+    return eventFactory;
 });
 
-gonnaWannaApp.service("ChannelService", function ($http) {
-    var channelService = this;
+gonnaWannaApp.factory("ChannelFactory", function ($http) {
+    var channelFactory = this;
 
-    channelService.GetAllChannels = function (callBack) {
+    channelFactory.GetAllChannels = function (callBack) {
         $http.get("/api/channels").success(function (data) {
             callBack(data);
         });
     };
 
-    channelService.GetChannel = function (channelID, callBack) {
+    channelFactory.GetChannel = function (channelID, callBack) {
         $http.get("/api/channels/" + channelID).success(function (data) {
             callBack(data);
         });
     };
 
-    return channelService;
+    return channelFactory;
 });
 
 
-gonnaWannaApp.service("LocationService", function ($http) {
-    var locationService = this;
+gonnaWannaApp.factory("LocationFactory", function ($http) {
+    var locationFactory = this;
 
-    locationService.GetAllLocations = function (callBack) {
+    locationFactory.GetAllLocations = function (callBack) {
         $http.get("/api/locations").success(function (data) {
             callBack(data);
         });
     };
 
-    locationService.GetLocation = function (locationID, callBack) {
+    locationFactory.GetLocation = function (locationID, callBack) {
         $http.get("/api/locations/" + locationID).success(function (data) {
             callBack(data);
         });
     };
 
-    return locationService;
+    return locationFactory;
 });
 
 
-gonnaWannaApp.service("SearchService", function ($http) {
-    var searchService = this;
+gonnaWannaApp.factory("SearchFactory", function ($http) {
+    var searchFactory = this;
 
-    searchService.SearchEventsAndChannels = function (searchQuery, callBack) {
+    searchFactory.SearchEventsAndChannels = function (searchQuery, callBack) {
         $http.get("/api/search/" + searchQuery).success(function (data) {
             callBack(data);
         });
@@ -120,5 +155,5 @@ gonnaWannaApp.service("SearchService", function ($http) {
     };
     */
 
-    return searchService;
+    return searchFactory;
 });
